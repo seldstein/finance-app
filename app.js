c512158971d717402e911948e73624cb9ce16749
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-06-02 18:07:01 -0500

    Add income source parameter

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const incomeValues = [];
+const incomeObjects = [];
 let totalIncome;
 
-function calcIncome(value) {
-  incomeValues.push(value);
+// Add a parameter for source name
+function addIncome(source, value) {
+    incomeObjects.push({source, value});
+    incomeValues.push(value);
 
-  totalIncome = incomeValues.reduce((total, current) => {
-    return (total += current);
-  }, 0);
-
-  console.log("TOTAL INCOME");
-  return totalIncome;
+    totalIncome = incomeValues.reduce((total, current) => {
+        return (total += current);
+      }, 0);
+    
+      console.log("TOTAL INCOME");
+      return totalIncome;
 }
